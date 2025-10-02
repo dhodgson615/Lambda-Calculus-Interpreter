31264d390a450d9a90e7055fea376e92c19c6291
dhodgson615 <dhodgson0615@gmail.com>
2025-10-02 13:57:33 -0400

    Refactor

--- src/_defs.py
+++ src/_defs.py
@@ -3,20 +3,22 @@ from __future__ import annotations
 from _expressions import Expression
 from _parser import Parser
 
-# δ‑definitions for logical connectives and arithmetic operations
-DEFS_SRC: dict[str, str] = {
-    "⊤": "λx.λy.x",
-    "⊥": "λx.λy.y",
-    "∧": "λp.λq.p q p",
-    "∨": "λp.λq.p p q",
-    "↓": "λn.λf.λx.n (λg.λh.h (g f)) (λu.x) (λu.u)",
-    "↑": "λn.λf.λx.f (n f x)",
-    "+": "λm.λn.m ↑ n",
-    "*": "λm.λn.m (+ n) 0",
-    "is_zero": "λn.n (λx.⊥) ⊤",
-    "-": "λm.λn.n ↓ m",
-    "≤": "λm.λn.is_zero (- m n)",
-    "pair": "λx.λy.λf.f x y",
+DEFS: dict[str, Expression] = {
+    name: Parser(src).parse()
+    for name, src in {
+        "⊤": "λx.λy.x",
+        "⊥": "λx.λy.y",
+        "∧": "λp.λq.p q p",
+        "∨": "λp.λq.p p q",
+        "↓": "λn.λf.λx.n (λg.λh.h (g f)) (λu.x) (λu.u)",
+        "↑": "λn.λf.λx.f (n f x)",
+        "+": "λm.λn.m ↑ n",
+        "*": "λm.λn.m (+ n) 0",
+        "is_zero": "λn.n (λx.⊥) ⊤",
+        "-": "λm.λn.n ↓ m",
+        "≤": "λm.λn.is_zero (- m n)",
+        "pair": "λx.λy.λf.f x y",
+    }.items()
 }
 
 DEFS: dict[str, Expression] = {}  # δ‑definitions
