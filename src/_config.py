27cfa5fb3cd7164dcadd43947ac8275216b9a022
dhodgson615 <dhodgson0615@gmail.com>
2025-10-02 14:00:30 -0400

    Refactor

--- src/_config.py
+++ src/_config.py
@@ -6,9 +6,4 @@ SHOW_STEP_TYPE: bool = True  # print “(δ)” or “(β)” after each step
 COMPACT: bool = True  # drop all spaces in printed terms
 DELTA_ABSTRACT: bool = True  # abstract Church numerals to digits
 RECURSION_LIMIT: int = -1  # recursion limit for deep reductions
-
-if RECURSION_LIMIT > 0:
-    sys.setrecursionlimit(RECURSION_LIMIT)
-
-else:
-    sys.setrecursionlimit(2**31 - 1)  # max recursion limit
+setrecursionlimit(RECURSION_LIMIT if RECURSION_LIMIT > 0 else 2**31 - 1)
