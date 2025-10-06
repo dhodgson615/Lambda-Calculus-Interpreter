4bec9632c7f20081dba017a7e45f31f0701c63aa
dhodgson615 <dhodgson0615@gmail.com>
2025-10-06 15:35:58 -0400

    Refactor

--- src/_config.py
+++ src/_config.py
@@ -1,9 +1,9 @@
 from sys import setrecursionlimit
 
-COLOR_PARENS: bool = True  # color‑matched parentheses by nesting level
-COLOR_DIFF: bool = False  # highlight the subterm(s) that changed
-SHOW_STEP_TYPE: bool = True  # print “(δ)” or “(β)” after each step
-COMPACT: bool = True  # drop all spaces in printed terms
-DELTA_ABSTRACT: bool = True  # abstract Church numerals to digits
-RECURSION_LIMIT: int = -1  # recursion limit for deep reductions
+COLOR_PARENS = True  # color‑matched parentheses by nesting level
+COLOR_DIFF = False  # highlight the subterm(s) that changed
+SHOW_STEP_TYPE = True  # print “(δ)” or “(β)” after each step
+COMPACT = True  # drop all spaces in printed terms
+DELTA_ABSTRACT = True  # abstract Church numerals to digits
+RECURSION_LIMIT = -1  # recursion limit for deep reductions
 setrecursionlimit(RECURSION_LIMIT if RECURSION_LIMIT > 0 else 2**31 - 1)
