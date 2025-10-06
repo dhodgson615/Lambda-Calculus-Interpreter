350f3677eec84fd4357cc048ab5eee5a1d38e368
dhodgson615 <dhodgson0615@gmail.com>
2025-10-06 14:56:11 -0400

    Refactor

--- src/test_lambda_calculus.py
+++ src/test_lambda_calculus.py
@@ -449,6 +449,7 @@ class TestNumericAbstraction:
 
             while True:
                 result = reduce_once(normal_expr, DEFS)
+
                 if not result:
                     break
                 normal_expr = result[0]
