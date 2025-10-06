c61fe4d19029f5caec37a20fce57ad82050ae674
dhodgson615 <dhodgson0615@gmail.com>
2025-10-06 14:56:14 -0400

    Refactor

--- src/test_lambda_calculus.py
+++ src/test_lambda_calculus.py
@@ -452,6 +452,7 @@ class TestNumericAbstraction:
 
                 if not result:
                     break
+
                 normal_expr = result[0]
 
             abstracted = abstract_numerals(normal_expr)
